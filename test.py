ecbae8be598619192fdecd8614db12447193fa86
kyuminKim <58509870+maybe0122@users.noreply.github.com>
2022-09-08 15:13:35 +0900

    컴그과제

---
+++ test.py
@@ -0,0 +1,68 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-300, 150)
+turtle.pendown()
+turtle.forward(100)
+turtle.right(135)
+turtle.forward(150)
+
+turtle.penup()
+turtle.goto(-150, 180)
+turtle.pendown()
+turtle.left(45)
+turtle.forward(150)
+
+turtle.penup()
+turtle.goto(-290, 17)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(90)
+#
+turtle.penup()
+turtle.goto(-100, 150)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(90)
+
+turtle.penup()
+turtle.goto(-110, 30)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(160)
+
+turtle.penup()
+turtle.goto(-70, 30)
+turtle.pendown()
+turtle.right(90)
+turtle.forward(100)
+
+turtle.penup()
+turtle.goto(10, 30)
+turtle.pendown()
+turtle.forward(100)
+#
+turtle.penup()
+turtle.goto(100, 150)
+turtle.pendown()
+turtle.left(90)
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+
+
+
+
+
+
+
+
+
